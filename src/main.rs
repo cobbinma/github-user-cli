7bf7e839c3e10048e3e12d22f8f4db6be2279a56
matthew.cobbing <matthew.cobbing@perkbox.com>
2021-01-05 18:00:12 +0000

    use clap to take username

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{App, Arg};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new("GitHub User CLI")
+        .version("0.1.0")
+        .author("M Cobbing <cobbinma@gmail.com>")
+        .about("find information about GitHub users")
+        .arg(
+            Arg::with_name("username")
+                .short("u")
+                .long("username")
+                .value_name("USERNAME")
+                .help("github username")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let username = matches.value_of("username").unwrap();
+
+    println!("{}", username);
+
+    Ok(())
 }
